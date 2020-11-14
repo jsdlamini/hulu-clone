4567b4ca8c0a0ee902b39c8931a23977be20b7ce
Johnson Dlamini <johnsjdsd@gmail.com>
2020-11-14 17:07:35 +0200

    navigation set up for movie catergories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import Header from "./Header";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
-     
       {/* Header */}
       <Header />
 
       {/* Nav */}
+      <Nav />
 
       {/* Results */}
     </div>
